e4dfc8deaefcaab3c476969551513544823950d1
jt <jiaan.t@gmail.com>
2017-08-04 01:39:44 -0400

    test

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/*initialize AOS animations*/
+AOS.init({
+  duration: 1200,
+});
+
+/*smooth scroll to anchor */
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1000);
+});
